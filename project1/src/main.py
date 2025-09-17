5136ffe4f1ee14b9a9af5c11816e480ce59e2997
CeduDev <cedric.neergaard@gmail.com>
2025-09-17 11:20:54 +0300

    studies: 🎓 updated structure

--- project1/src/main.py
+++ project1/src/main.py
@@ -3,13 +3,13 @@ import os
 import json
 from datetime import datetime
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
-sys.path.append(os.path.join(os.path.dirname(__file__), '../..', 'OpenCEP'))
+# sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
+# sys.path.append(os.path.join(os.path.dirname(__file__), "../..", "OpenCEP"))
 dataset_dir = os.path.join(os.path.dirname(__file__), "..", "datasets", "2025-05-data")
 
-from OpenCEP.CEP import CEP
-from OpenCEP.base.Event import Event
-from OpenCEP.stream.Stream import Stream
+from CEP import CEP
+from base.Event import Event
+from stream.Stream import Stream
 from pattern import bike_hot_path_pattern, simple_bike_trip_pattern
 from file_reader import csv_to_stream
 from base.DataFormatter import DataFormatter, EventTypeClassifier
@@ -19,16 +19,17 @@ class MyEventTypeClassifier(EventTypeClassifier):
     def get_event_type(self, event_payload: dict):
         return "BikeTrip"
 
+
 class MyDataFormatter(DataFormatter):
     def __init__(self):
         super().__init__(event_type_classifier=MyEventTypeClassifier())
-    
+
     def parse_event(self, raw_data: str):
         return json.loads(raw_data)
-    
+
     def get_event_timestamp(self, payload: dict):
         return datetime.fromisoformat(payload["started_at"])
-    
+
     def get_probability(self, payload: dict):
         return None
 
@@ -52,4 +53,3 @@ cep.run(in_stream, out_stream, formatter)
 print(out_stream.count())
 print(out_stream.first())
 print(out_stream.last())
-
