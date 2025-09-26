3739db48e250719c6ae2d09a64c8f29333666b81
CeduDev <cedric.neergaard@gmail.com>
2025-09-26 21:34:18 +0300

    studies: 🎓 add option to switch between switch files to use and how many rows to read, add logs, formatting

--- project1/src/file_reader.py
+++ project1/src/file_reader.py
@@ -2,16 +2,36 @@ import csv
 import json
 from datetime import datetime
 
-def csv_to_stream(filepath, in_stream):
-    """Stream CSV rows directly to input stream"""
-    with open(filepath, 'r') as f:
+
+def csv_to_stream(filepath, in_stream, rows_to_take=None):
+    if rows_to_take != None:
+        print("Taking the first " + str(rows_to_take) + " rows of the csv file")
+    else:
+        print("Taking all the rows of the csv file")
+
+    rows_taken = 0
+
+    with open(filepath, "r") as f:
         reader = csv.DictReader(f)
         for row in reader:
+            if rows_to_take != None and rows_taken >= rows_to_take:
+                break
             # Convert to JSON string and add to stream
-            if row["ride_id"] and row["start_station_id"] and row["end_station_id"] and row["started_at"]:
-                in_stream.add_item(json.dumps({
-                    "bike_id": row["ride_id"],
-                    "start_station": row["start_station_id"],
-                    "end_station": row["end_station_id"],
-                    "started_at": row["started_at"]
-                }))
+            if rows_to_take != None and rows_taken < rows_to_take:
+                if (
+                    row["ride_id"]
+                    and row["start_station_id"]
+                    and row["end_station_id"]
+                    and row["started_at"]
+                ):
+                    in_stream.add_item(
+                        json.dumps(
+                            {
+                                "bike_id": row["ride_id"],
+                                "start_station": row["start_station_id"],
+                                "end_station": row["end_station_id"],
+                                "started_at": row["started_at"],
+                            }
+                        )
+                    )
+                rows_taken += 1
--- project1/src/main.py
+++ project1/src/main.py
@@ -1,32 +1,29 @@
-import sys
 import os
 import json
 from datetime import datetime
 
-# sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
-# sys.path.append(os.path.join(os.path.dirname(__file__), "../..", "OpenCEP"))
 dataset_dir = os.path.join(os.path.dirname(__file__), "..", "datasets", "2025-05-data")
 
 from CEP import CEP
-from base.Event import Event
 from stream.Stream import Stream
 from pattern import bike_hot_path_pattern
 from file_reader import csv_to_stream
 from base.DataFormatter import DataFormatter, EventTypeClassifier
 
 
-class MyEventTypeClassifier(EventTypeClassifier):
+class BikeTripEventTypeClassifier(EventTypeClassifier):
     def get_event_type(self, event_payload: dict):
         return "BikeTrip"
 
 
-class MyDataFormatter(DataFormatter):
+class BikeTripDataFormatter(DataFormatter):
     def __init__(self):
-        super().__init__(event_type_classifier=MyEventTypeClassifier())
+        super().__init__(event_type_classifier=BikeTripEventTypeClassifier())
 
     def parse_event(self, raw_data: str):
         return json.loads(raw_data)
 
+    # Use the "started_at"-column as the event timestamp
     def get_event_timestamp(self, payload: dict):
         return datetime.fromisoformat(payload["started_at"])
 
@@ -34,21 +31,33 @@ class MyDataFormatter(DataFormatter):
         return None
 
 
-pattern = bike_hot_path_pattern()
+# filename_endStations_rowsToTake = ["test_data.csv", [7.0, 8.0, 9.0], None]
+filename_endStations_rowsToTake = ["202505-citibike-tripdata_1.csv", [5569.06], 40]
+print(
+    "Using filename "
+    + filename_endStations_rowsToTake[0]
+    + " with end stations: "
+    + ", ".join(map(str, filename_endStations_rowsToTake[1]))
+)
 
-# Create streams
+pattern = bike_hot_path_pattern(filename_endStations_rowsToTake[1])
+
 in_stream = Stream()
 out_stream = Stream()
 
+file_path = os.path.join(dataset_dir, filename_endStations_rowsToTake[0])
 
-file_path = os.path.join(dataset_dir, "test_data.csv")
-csv_to_stream(file_path, in_stream)
+print("Reading input file: " + filename_endStations_rowsToTake[0])
+csv_to_stream(file_path, in_stream, filename_endStations_rowsToTake[2])
 in_stream.close()
+print("File read, closing stream")
 
-formatter = MyDataFormatter()
+formatter = BikeTripDataFormatter()
 
+print("Start CEP engine and start the run")
 cep = CEP(patterns=[pattern])
-cep.run(in_stream, out_stream, formatter)
+total_time = cep.run(in_stream, out_stream, formatter)
+print("Engine done running, ran for " + str(total_time) + " seconds")
 
 for match in out_stream:
     print(match)
