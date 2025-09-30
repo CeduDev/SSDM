e6ee06e2b906444989bea5bd2ea6125905f7ef5a
CeduDev <cedric.neergaard@gmail.com>
2025-09-30 18:11:01 +0300

    studies: 🎓 add a file sink

--- project1/code/src/main/java/org/myorg/mycep/BikeHotPathJob.java
+++ project1/code/src/main/java/org/myorg/mycep/BikeHotPathJob.java
@@ -2,12 +2,19 @@ package org.myorg.mycep;
 
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.OnCheckpointRollingPolicy;
 import org.apache.flink.util.Collector;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.cep.CEP;
 import org.apache.flink.cep.PatternStream;
 import org.apache.flink.cep.functions.PatternProcessFunction;
 import org.apache.flink.cep.pattern.Pattern;
+import org.apache.flink.connector.file.sink.FileSink;
+import org.apache.flink.core.fs.Path;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -19,11 +26,15 @@ public class BikeHotPathJob {
         // Enable latency tracking every 1 sec, working? Idk
         env.getConfig().setLatencyTrackingInterval(1000);
 
-        List<Float> endStations = Arrays.asList(7.0f, 8.0f, 9.0f);
+        // Small test data
+        // String dataFilePath = "../datasets/";
+        // List<Float> endStations = Arrays.asList(7.0f, 8.0f, 9.0f);
+        // DataStream<BikeTripEvent> trips = ReadCSVData.fromCsv(env, dataFilePath, "test_data.csv");
 
-        // Create data from the fileName
-        String dataFilePath = "../datasets/";
-        DataStream<BikeTripEvent> trips = ReadCSVData.fromCsv(env, dataFilePath, "test_data.csv");
+        // January 1 data
+        String dataFilePath = "../datasets/2019-data/2019-citibike-tripdata/1_January/";
+        List<Float> endStations = Arrays.asList(3283.0f, 518.0f, 3154.0f);
+        DataStream<BikeTripEvent> trips = ReadCSVData.fromCsv(env, dataFilePath, "201901-citibike-tripdata_1.csv");
 
         // Use the pattern
         Pattern<BikeTripEvent, ?> pattern = BikeTripPattern.hotPathPattern(endStations);
@@ -41,17 +52,43 @@ public class BikeHotPathJob {
                     Collector<String> out) throws Exception {
                 List<BikeTripEvent> aTrips = pattern.get("a");
                 BikeTripEvent b = pattern.get("b").get(0);
-                String res = "Hot path detected! Stations in order are: ";
+                
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                
+                String res = "================\nHot path detected! Stations and times in order are\n";
+                
                 for (BikeTripEvent a: aTrips) {
-                    res += a.startStation + " -> " + a.endStation + ", ";
+                    String startTimeStr = LocalDateTime.ofEpochSecond(a.startTime / 1000, 0, ZoneOffset.UTC).format(formatter);
+                    String endTimeStr = LocalDateTime.ofEpochSecond(a.endTime / 1000, 0, ZoneOffset.UTC).format(formatter);
+
+                    res += String.format("%s -> %s, times: %s -> %s\n",
+                        a.startStation,
+                        a.endStation,
+                        startTimeStr,
+                        endTimeStr
+                    );
                 }
-                res += b.endStation;
+
+                res += String.format("%s -> %s, times: %s -> %s\n", b.startStation, b.endStation, LocalDateTime.ofEpochSecond(b.startTime / 1000, 0, ZoneOffset.UTC).format(formatter), LocalDateTime.ofEpochSecond(b.endTime / 1000, 0, ZoneOffset.UTC).format(formatter));
+                res += "================";
 
                 out.collect(res);
         }});
 
+        // Define the sink
+        FileSink<String> sink = FileSink
+            .forRowFormat(
+                new Path("./project1/output/"),      // directory where files are written
+                new SimpleStringEncoder<String>("UTF-8")
+            )
+            .withRollingPolicy(OnCheckpointRollingPolicy.build()) // not sure what this is honestly :D
+            .build();
+        
+        // Print to the sink defined above
+        result.sinkTo(sink);
+
         // Print to stdout in Flink (not in your own terminal, see the README.md for more details)
-        result.print();
+        // result.print();
 
         env.execute("Bike Hot Path CEP Job");
     }
